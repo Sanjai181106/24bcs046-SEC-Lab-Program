6900bfdced287e849edc9d6c48cc2adae7645ce8
Sanjai181106 <24bcs046@kgcas.com>
2025-07-25 12:59:21 +0530

    Add files via upload

---
+++ 24bcs046-bcs cs-SEC-lab/data types/script.js
@@ -0,0 +1,26 @@
+let name1="Sanjai.S";
+console.log(name1);
+console.log(typeof name1);
+
+let age=18;
+console.log(age);
+console.log(typeof age);
+
+let stock=false;
+console.log(stock);
+console.log(typeof stock);
+
+let location1;
+console.log(location);
+
+let colors = ["blue","red","green"];
+console.table(colors);
+console.log(typeof colors);
+
+let marks = {
+    tamil:89,
+    english:74,
+    maths:68,
+}
+console.log(marks);
+console.log(typeof marks);
